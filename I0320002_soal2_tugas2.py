05d998caead2feb961d1b19b35894926008d5d27
Adrian K <adrian.kwanadi@gmail.com>
2021-03-12 01:06:04 +0700

    Update Soal 2 Tugas 2

---
+++ I0320002_soal2_tugas2.py
@@ -0,0 +1,33 @@
+#input data diri
+nama = "Adrian Kwanadi Setiono"
+nama_panggilan = "Adrian"
+tempat_tinggal = "Harapan Indah 2 Blok HO 2 No. 121, Bekasi, Jawa Barat"
+tempat_lahir = "Jakarta"
+tanggal_lahir = "14 Desember 2002"
+umur = "18 tahun"
+print("Perkenalkan nama saya", str(nama) + ",", "yang biasa dipanggil", str(nama_panggilan) + ".")
+print("Saya tinggal di", str(tempat_tinggal) + ".")
+print("Saya lahir di", str(tempat_lahir) + ",", str(tanggal_lahir) + ".")
+print("Umur saya sekarang", str(umur) + ".")
+
+#input data kesehatan
+berat_badan = int(122/2)
+tinggi_badan = float(875 / 5)
+alergi = "telur"
+dampak_alergi = "jerawat"
+riwayat_penyakit = "campak"
+print("Berat badan saya adalah", berat_badan, "kg.")
+print("Tinggi badan saya adalah", tinggi_badan, "cm.")
+print("Saya mempunyai alergi terhadap", str(alergi) + ".", "Jika saya memakan", str(alergi) + ",", "muka saya akan timbul berjerawat.")
+print("Riwayat penyakit yang pernah miliki adalah", str(dampak_alergi) + ".")
+
+#input data diri lainnya
+universitas = "Universitas Sebelas Maret"
+fakultas = "Teknik"
+prodi = "Industri"
+angkatan = "2020"
+NIM = "I0320002"
+print("Saat ini, saya berkuliah di", str(universitas) + ".")
+print("Jurusan yang saya pilih adalah Fakultas", fakultas, str(prodi) + ".")
+print("Saya dari angkatan", str(angkatan) + ".")
+print("NIM saya adalah", str(NIM) + ".")
