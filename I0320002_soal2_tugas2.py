7d3a569f874df4eb7ac9ad837f7f30cda452730f
Adrian K <adrian.kwanadi@gmail.com>
2021-03-12 21:14:03 +0700

    Update Soal 2 Tugas 2

--- I0320002_soal2_tugas2.py
+++ I0320002_soal2_tugas2.py
@@ -5,19 +5,23 @@ tempat_tinggal = "Harapan Indah 2 Blok HO 2 No. 121, Bekasi, Jawa Barat"
 tempat_lahir = "Jakarta"
 tanggal_lahir = "14 Desember 2002"
 umur = "18 tahun"
+hobi = "baca novel dan nonton"
 print("Perkenalkan nama saya", str(nama) + ",", "yang biasa dipanggil", str(nama_panggilan) + ".")
 print("Saya tinggal di", str(tempat_tinggal) + ".")
 print("Saya lahir di", str(tempat_lahir) + ",", str(tanggal_lahir) + ".")
+print("Hobi saya adalah", str(hobi) + ".")
 print("Umur saya sekarang", str(umur) + ".")
 
 #input data kesehatan
 berat_badan = int(122/2)
 tinggi_badan = float(875 / 5)
+ukuran_sepatu = int(86 / 2)
 alergi = "telur"
 dampak_alergi = "jerawat"
 riwayat_penyakit = "campak"
 print("Berat badan saya adalah", berat_badan, "kg.")
 print("Tinggi badan saya adalah", tinggi_badan, "cm.")
+print("Ukuran sepatu saya adalah", str(ukuran_sepatu) + ".")
 print("Saya mempunyai alergi terhadap", str(alergi) + ".", "Jika saya memakan", str(alergi) + ",", "muka saya akan timbul berjerawat.")
 print("Riwayat penyakit yang pernah miliki adalah", str(dampak_alergi) + ".")
 
